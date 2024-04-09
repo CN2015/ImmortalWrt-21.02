3b6f7d6a158d0d1ed0f54859e59b6c8d71ba84ae
CN2014 <oucezuo@qq.com>
2024-04-09 20:57:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,6 +15,35 @@ sed -i "s/hostname='ImmortalWrt'/hostname='OpenWrt'/g" package/base-files/files/
 # Modify hostname
 #sed -i 's/OpenWrt/360T7/g' package/base-files/files/bin/config_generate
 
+# 修改时区 UTF-8
+sed -i 's/UTC/CST-8/g'  package/base-files/files/bin/config_generate
+
+# 时区
+sed -i 's/time1.apple.com/time1.cloud.tencent.com/g'  package/base-files/files/bin/config_generate
+sed -i 's/time1.google.com/ntp.aliyun.com/g'  package/base-files/files/bin/config_generate
+sed -i 's/time.cloudflare.com/cn.ntp.org.cn/g'  package/base-files/files/bin/config_generate
+sed -i 's/pool.ntp.org/cn.pool.ntp.org/g'  package/base-files/files/bin/config_generate
+
+# 替换源 
+sed -i 's,mirrors.vsean.net/openwrt,mirrors.pku.edu.cn/immortalwrt,g'  package/emortal/default-settings/files/99-default-settings-chinese
+
+# 
+MTK_HNAT(){
+  rm -rf target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_hnat
+  svn co https://github.com/padavanonly/immortalwrtARM/branches/mt7981/target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_hnat
+  mv mtk_hnat target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_hnat
+}
+
+MT_WIFI(){
+  rm -rf package/mtk/drivers/mt_wifi
+  svn co https://github.com/padavanonly/immortalwrtARM/branches/mt7981/package/mtk/drivers/mt_wifi
+  mv mt_wifi package/mtk/drivers/mt_wifi
+}
+
+MTK_HNAT
+echo '-------------------------------------------------------------------------------------------------'
+MT_WIFI
+
 # 修改插件名字
 sed -i 's/"Release Ram"/"内存释放"/g' `egrep "Release Ram" -rl ./`
 sed -i 's/"Custom Commands"/"命令脚本"/g' `egrep "Custom Commands" -rl ./`
